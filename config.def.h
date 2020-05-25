45c1ae613a3e9d1f254a8c924e6e3bc7e0721790
Fred Fredburger <fredfredburger86@protonmail.com>
2020-05-25 22:26:35 +0300

    use correct fg/bg/cur colors

--- config.def.h
+++ config.def.h
@@ -127,10 +127,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 256;
 
 /*
  * Default shape of cursor
