ef1c3c41f7b284546a18c4f6b32fde4a9fd614de
Fred Fredburger <fredfredburger86@protonmail.com>
2020-05-29 20:32:10 +0300

    always use black bg regardless of xrdb for better contrast

--- config.def.h
+++ config.def.h
@@ -117,6 +117,7 @@ static const char *colorname[] = {
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
+	"black",
 	"#cccccc",
 	"#555555",
 	"black",
@@ -128,7 +129,7 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
+unsigned int defaultbg = 259;
 static unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 256;
 
